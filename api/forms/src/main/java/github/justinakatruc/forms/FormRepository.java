6c905540ac038e7777b59928e486bb7382b39fe6
Truc Minh Nguyen <minhtruc103@gmail.com>
2025-02-17 23:47:39 +0700

    Remove API, no longer needed

--- api/forms/src/main/java/github/justinakatruc/forms/FormRepository.java
+++
@@ -1,9 +0,0 @@
-package github.justinakatruc.forms;
-
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FormRepository extends MongoRepository<Form, ObjectId> {
-}
